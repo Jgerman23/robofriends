ddccff059245b04466754e179721d339e395900b
Justin German <justin.german23@gmail.com>
2019-12-01 18:04:40 -0600

    cleaned up js, created App js file

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ({name, email, id}) => {
     return (
         <div className="bg-light-green dib br3 ma2 bw2 grow shadow-5 tc">
-            <img src={`https://robohash.org/${props.id}?200x200`} alt=""/>
+            <img src={`https://robohash.org/${id}?200x200`} alt=""/>
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
