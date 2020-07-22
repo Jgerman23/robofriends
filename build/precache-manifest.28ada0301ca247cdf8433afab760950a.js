d4390abbebb9d7313a4a52c1265af15a880ffaf0
JGerman23 <justin.german23@gmail.com>
2020-07-22 10:36:36 -0500

    gh-pages deploy

---
+++ build/precache-manifest.28ada0301ca247cdf8433afab760950a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93edef0646691f27686c0b84f60bb15e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "1710a531087a163bc351",
+    "url": "/robofriends/static/css/main.d05d2c3b.chunk.css"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/robofriends/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1710a531087a163bc351",
+    "url": "/robofriends/static/js/main.c9877c3d.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
